150deb1d4ad355387462158c02486a7a579f06dd
becherj0489 <becherj0489@my.uwstout.edu>
2017-04-26 14:36:03 -0500

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -10,13 +10,12 @@
 // Defined as constants so that they can't be changed
 DEFINE ('DB_USER', 'ceelabadmin');
 DEFINE ('DB_PASSWORD', 'databaseoverlord');
-DEFINE ('DB_HOST', 'http://localhost/phpmyadmin/sql.php?server=1&db=cee_lab_invetory_database&table=equipment_manuals&pos=0&token=b1986a0f2d002d64fc995e309dcc9aee');
 DEFINE ('DB_NAME', 'invetory');
 
 // $dbc will contain a resource link to the database
 // @ keeps the error from showing in the browser
 
-$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+$dbc = @mysqli_connect($SERVER_NAME, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL: ' .
 mysqli_connect_error());
 ?>
